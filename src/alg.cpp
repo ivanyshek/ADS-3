87c7b3b3e2ae24fe2f7101f3e31788089895666f
ivanyshek <105326502+ivanyshek@users.noreply.github.com>
2024-03-16 14:44:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 
 int cbinsearch(int *arr, int size, int value) {
     int result = 0;
-    for (int i = 0;i < size;i++) {
+    for (int i = 0; i < size; i++) {
         if (arr[i] == value) result++;
     }
     return result;
